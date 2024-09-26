dd25bc78ceae451df96f676790dbed9822dbba67
Blank <wangfj11@foxmail.com>
2024-09-26 16:03:22 +0800

    [增加]1. 增加FUI的包的入口

---
+++ Runtime/GameApp.FairyGUIPackage.cs
@@ -0,0 +1,25 @@
+﻿#if ENABLE_GAME_FRAME_X_UI_FAIYGUI
+using GameFrameX.Runtime;
+using GameFrameX.UI.FairyGUI.Runtime;
+
+public static partial class GameApp
+{
+    /// <summary>
+    /// 获取UI包管理组件。
+    /// </summary>
+    public static FairyGUIPackageComponent FUIPackage
+    {
+        get
+        {
+            if (_fUIPackage == null)
+            {
+                _fUIPackage = GameEntry.GetComponent<FairyGUIPackageComponent>();
+            }
+
+            return _fUIPackage;
+        }
+    }
+
+    private static FairyGUIPackageComponent _fUIPackage;
+}
+#endif
