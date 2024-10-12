6a6554df6c889594f8f91f79d2938054e62f5e02
Blank <wangfj11@foxmail.com>
2024-10-12 18:21:54 +0800

    [增加]1. 增加XLUA组件

---
+++ Runtime/GameApp.XLua.cs
@@ -0,0 +1,25 @@
+﻿#if ENABLE_GAME_FRAME_X_X_LUA
+using GameFrameX.XLua.Runtime;
+using GameFrameX.Runtime;
+
+public static partial class GameApp
+{
+    /// <summary>
+    /// 获取XLua组件。
+    /// </summary>
+    public static XLuaComponent XLua
+    {
+        get
+        {
+            if (_xlua == null)
+            {
+                _xlua = GameEntry.GetComponent<XLuaComponent>();
+            }
+
+            return _xlua;
+        }
+    }
+
+    private static XLuaComponent _xlua;
+}
+#endif
